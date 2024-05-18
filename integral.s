57cfa3aad1c65df37e97bb9fb887fe62347b18b8
zuzdud <duda.zuzanna7@gmail.com>
2024-05-18 22:30:24 +0000

    cos liczy ale tylko raz

--- integral.s
+++ integral.s
@@ -5,7 +5,8 @@
 	b: .float 10
 	n: .float 1000
 	two: .float 2
-
+	six: .float 6
+	z: .float 0
 .section .text
 
 _start:
@@ -26,5 +27,17 @@ fld %ST(1)
 fadd %ST(1)
 fdiv two #r4 = x
 
-fmul %ST(2), %ST(0)
+fld %ST(0)
+fmul %ST(1), %ST(0)
+fsub six #r3 pierwsza czesc wyniku
 
+fld %ST(1)
+fsub two # r2 druga czesc wyniku
+
+fld %ST(1)
+fdiv %ST(1), %ST(0) # podzielic
+
+fmul %ST(6), %ST(0) #pomnozyc razy i wynik w r1
+fadd %ST(0)
+
+fmul z, %ST(4)
